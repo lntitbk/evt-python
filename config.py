eb5a1c0f366b986d9118d0d55158779a6cf1d02b
Trinh Le <lnt.itbk@gmail.com>
2022-01-14 15:27:26 +0700

    init project

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/db'
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    ENV = 'production'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/db'
+    SQLALCHEMY_ECHO = False
+
